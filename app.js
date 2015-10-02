73a39ee8dde46ea4c55435cbccd467dddf2724d4
yomotsu <oyamada@pxgrid.com>
2015-10-02 15:47:28 +0900

    change port

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-var io = require( 'socket.io' ).listen( 3002 );
+// var port = process.env.PORT || 3002;
+var port = 80; // Azure Web Apps only listens on ports 80 & 443
+var io = require( 'socket.io' ).listen( port );
+
+  io.set('transports', ['websocket','xhr-polling']);
+  io.set("polling duration", 10);
+
+console.log( port );
 
 io.on( 'connection', function ( socket ) {
 
